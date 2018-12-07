9d8e38acee4d49f89a50345a56c02882f497a3ba
Kexin <kexincuichina@gmail.com>
2018-12-07 15:32:44 -0600

    add problem description and time complexity

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,12 @@
+/** 1. Two Sum (Easy)
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+(Example)
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Arrays;
@@ -37,3 +46,11 @@ class TwoSum {
 		System.out.println(Arrays.toString(res));
 	}
 }
+
+// One-pass Hash Table
+    // while we iterate & insert elements into the table,
+    // we also look back to check if current element's complement 
+    // already exists in the table. 
+    // If so, we have found a solution and return it immediately. 
+// Time: O(n)
+// Space: O(n)
